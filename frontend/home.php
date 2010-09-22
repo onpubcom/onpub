ffe92ada52abccfeb4d90e47fe59ca1cf1afb63f
Corey Taylor <corey@onpub.com>
2010-09-22 03:20:50 +0000

    Default home page layout is now 2 columns with What's New being in the column on the right Default home page article now no longer shows the Title field. This makes it easier to create a totally custom home page layout

--- frontend/home.php
+++ frontend/home.php
@@ -9,45 +9,48 @@
  * as published by the Free Software Foundation; version 2.
  */
 
-en('<div class="yui3-g">');
-en('<div class="yui3-u-23-24">');
-
 if ($onpub_website) {
-  if ($onpub_disp_article) {
-    $onpub_article = $onpub_articles->get($onpub_disp_article);
-
-    if ($onpub_article) {
-      en('<h1>' . $onpub_article->title . '</h1>');
-      en($onpub_article->content);
-    }
-    else {
-      br();
-      en('<p><a href="' . $onpub_dir_root . $onpub_dir_manage . 'index.php?onpub=NewArticle" target="_blank">Create a new article</a> to customize this page.</p>');
-    }
-  }
   if ($onpub_disp_updates) {
+    en('<div class="yui3-g">');
+    en('<div class="yui3-u-2-3">');
+    
+    if ($onpub_disp_article) {
+      $onpub_article = $onpub_articles->get($onpub_disp_article);
+    
+      if ($onpub_article) {
+        en($onpub_article->content);
+      }
+      else {
+        br();
+        en('<p><a href="' . $onpub_dir_root . $onpub_dir_manage . 'index.php?onpub=NewArticle" target="_blank">Create a new article</a> to customize this page.</p>');
+      }
+    }
+    
+    en('</div>');
+    en('<div class="yui3-u-1-3">');
+
     $qo = new OnpubQueryOptions();
     $qo->includeContent = true;
     $qo->orderBy = 'created';
     $qo->order = 'DESC';
     $qo->rowLimit = 6;
-
+  
     $articles = $onpub_articles->select($qo);
 
     if (sizeof($articles) > 1) {
       en('<h1>What\'s New</h1>');
-
+  
       foreach ($articles as $a) {
         if ($a->ID != $onpub_disp_article) {
           $samaps = $onpub_samaps->select(null, null, $a->ID);
-
+  
           if (sizeof($samaps)) {
             if (in_array($samaps[0]->sectionID, $sectIDs)) {
               // Only include sectionID in links if current section is visible.
               en('<h2><a href="index.php?sectionID=' . $samaps[0]->sectionID . '&amp;articleID=' . $a->ID . '">' . $a->title . '</a></h2>');
-
+  
               en('<p><em>' . $a->getCreated()->format('M j, Y') . '</em>');
-
+  
               if ($a->getSummary()) {
                 en(' &ndash; ' . $a->getSummary() . '...<a href="index.php?sectionID=' . $samaps[0]->sectionID . '&amp;articleID=' . $a->ID . '"><img src="' . $onpub_dir_root . $onpub_dir_frontend . 'images/bullet_go.png" width="16" height="16" alt="Read more." title="Read more." align="top"></a><a href="index.php?sectionID=' . $samaps[0]->sectionID . '&amp;articleID=' . $a->ID . '">Read more</a></p>');
               }
@@ -57,9 +60,9 @@ if ($onpub_website) {
             }
             else {
               en('<h2><a href="index.php?articleID=' . $a->ID . '">' . $a->title . '</a></h2>');
-
+  
               en('<p><em>' . $a->getCreated()->format('M j, Y') . '</em>');
-
+  
               if ($a->getSummary()) {
                 en(' &ndash; ' . $a->getSummary() . '...<a href="index.php?articleID=' . $a->ID . '"><img src="' . $onpub_dir_root . $onpub_dir_frontend . 'images/bullet_go.png" width="16" height="16" alt="Read more." title="Read more." align="top"></a><a href="index.php?articleID=' . $a->ID . '">Read more</a></p>');
               }
@@ -70,9 +73,9 @@ if ($onpub_website) {
           }
           else {
             en('<h2><a href="index.php?articleID=' . $a->ID . '">' . $a->title . '</a></h2>');
-
+  
             en('<p><em>' . $a->getCreated()->format('M j, Y') . '</em>');
-
+  
             if ($a->getSummary()) {
               en(' &ndash; ' . $a->getSummary() . '...<a href="index.php?articleID=' . $a->ID . '"><img src="' . $onpub_dir_root . $onpub_dir_frontend . 'images/bullet_go.png" width="16" height="16" alt="Read more." title="Read more." align="top"></a><a href="index.php?articleID=' . $a->ID . '">Read more</a></p>');
             }
@@ -83,15 +86,27 @@ if ($onpub_website) {
         }
       }
     }
+
+    en('</div>');
+    en('</div>');
+  }
+  else {
+    if ($onpub_disp_article) {
+      $onpub_article = $onpub_articles->get($onpub_disp_article);
+    
+      if ($onpub_article) {
+        en($onpub_article->content);
+      }
+      else {
+        br();
+        en('<p><a href="' . $onpub_dir_root . $onpub_dir_manage . 'index.php?onpub=NewArticle" target="_blank">Create a new article</a> to customize this page.</p>');
+      }
+    }
   }
 }
 else {
   br();
   en('<p>Onpub\'s installed. <a href="' . $onpub_dir_root . $onpub_dir_manage . 'index.php?onpub=NewWebsite" target="_blank">Create a new website</a> and then reload this page to get started.</p>');
 }
-
-en('</div>');
-en('<div class="yui3-u-1-24"></div>');
-en('</div>');
 
 ?>
