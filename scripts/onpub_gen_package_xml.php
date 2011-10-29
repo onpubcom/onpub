72e38019117084023388e6674633e93b11741373
Corey Taylor <corey@onpub.com>
2011-10-29 19:46:42 +0000

    Added script to autogen PEAR package file.

---
+++ scripts/onpub_gen_package_xml.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('PEAR/PackageFileManager2.php');
+
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$pfm = PEAR_PackageFileManager2::importOptions('./package-template.xml');
+
+$pfm_options = array('packagedirectory' => 'onpub/',
+                     'baseinstalldir' => 'Onpub/');
+
+$pfm->setOptions($pfm_options);
+
+$pfm->generateContents();
+
+$pfm->writePackageFile();
+
+?>
