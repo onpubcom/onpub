b91e54d5c570c8c942b558bd3bb9df14b00cb199
Corey Taylor <corey@onpub.com>
2011-10-30 18:44:35 +0000

    Fixed path to Onpub schema file.

--- api/classes/OnpubDatabase.php
+++ api/classes/OnpubDatabase.php
@@ -47,7 +47,7 @@ class OnpubDatabase
     $line = 0;
     $exceptions = array ();
 
-    $sqlfile = file('../api/sql/deleteonpubtables.sql');
+    $sqlfile = file('../../data/api/sql/deleteonpubtables.sql');
 
     // advance past all comments
     while (strpos($sqlfile[$line], '--') !== FALSE) {
@@ -110,7 +110,12 @@ class OnpubDatabase
     $line = 0;
     $exceptions = array ();
 
-    $sqlfile = file('../api/sql/createonpubtables-rev0.sql');
+    if (file_exists('../../data/api/sql/createonpubtables-rev0.sql') {
+      $sqlfile = file('../../data/api/sql/createonpubtables-rev0.sql');
+    }
+    else {
+      $sqlfile = file('../../../data/Onpub/data/api/sql/createonpubtables-rev0.sql');
+    }
 
     // advance past all comments
     while (strpos($sqlfile[$line], '--') !== FALSE) {
