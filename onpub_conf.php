1dd820caa2e03e3124da3ac8e9a9980aa421ee2f
Onpub.com <info@onpub.com>
2012-09-22 12:29:51 -0400

    Updated YUI CDN version to 3.7.1 as well.

--- onpub_conf.php
+++ onpub_conf.php
@@ -250,6 +250,6 @@ variable will usually be set to whatever the most recent version of YUI 3 is at
 the time new versions of Onpub are released.
 */
 if (!isset($onpub_yui_version))
-  $onpub_yui_version = '3.5.1';
+  $onpub_yui_version = '3.7.1';
 
 ?>
