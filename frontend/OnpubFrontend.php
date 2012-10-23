7319e18dc7f1f2d5105efb3415d62262c2a6c7dc
onpubcom <info@onpub.com>
2012-10-23 15:31:18 -0400

    Added new viewport meta tag which enables mobile layout to be scaled automatically to devices screen width.

--- frontend/OnpubFrontend.php
+++ frontend/OnpubFrontend.php
@@ -249,6 +249,7 @@ class OnpubFrontend
 
   protected function meta()
   {
+  	en('<meta name="viewport" content="width=device-width; initial-scale=1.0">');
     en('<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">');
     en('<meta http-equiv="Content-Style-Type" content="text/css">');
 
