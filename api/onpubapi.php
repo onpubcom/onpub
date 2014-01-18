55fa3d913f31cce8bef1b87930eb59b07aae273f
onpubcom <info@onpub.com>
2014-01-18 12:09:55 -0500

    Started work on Onpub 1.6. Set all external deps to their latest stable release versions.

--- api/onpubapi.php
+++ api/onpubapi.php
@@ -9,7 +9,7 @@
  * as published by the Free Software Foundation; version 2.
  */
 
-define("ONPUBAPI_VERSION", "1.5");
+define("ONPUBAPI_VERSION", "1.6");
 define("ONPUBAPI_SCHEMA_VERSION", 1);
 
 // phpThumb
